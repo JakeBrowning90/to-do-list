08cf51e57006326305515e60776371e34f8518d5
Jake Browning <jacobbrowning90@gmail.com>
2022-12-12 12:07:43 +0000

    Add grid display to task details area on list view

--- src/fillTaskDesc.js
+++ src/fillTaskDesc.js
@@ -6,12 +6,28 @@ import { drawFormView } from "./drawFormView";
 
 function fillTaskDesc(title, desc, priority, category, dueDate, indexPosition) {
     const taskDescColumn = document.querySelector('.taskDescColumn');
-    taskDescColumn.textContent = title + ": "+ desc + priority + category + dueDate;
+    taskDescColumn.textContent = null;
+    const taskDetailName = document.createElement('div');
+    taskDetailName.classList.add('taskDetailName');
+    taskDetailName.textContent = title;
+    const taskDetailCategory = document.createElement('div');
+    taskDetailCategory.classList.add('taskDetailCategory');
+    taskDetailCategory.textContent = category;
+    const taskDetailPriority = document.createElement('div');
+    taskDetailPriority.classList.add('taskDetailPriority');
+    taskDetailPriority.textContent = priority;
+    const taskDetailDueDate = document.createElement('div');
+    taskDetailDueDate.classList.add('taskDetailDueDate');
+    taskDetailDueDate.textContent = dueDate;
+    const taskDetailDescription = document.createElement('div');
+    taskDetailDescription.classList.add('taskDetailDescription');
+    taskDetailDescription.textContent = desc;
 
     const editTaskBtn = document.createElement('button');
+    editTaskBtn.classList.add('editTaskBtn');
     editTaskBtn.textContent = "Edit";
     editTaskBtn.addEventListener("click", function () {
-        console.log("EDIT!");
+        //console.log("EDIT!");
         clearView();
         //TO-DO: New function with modified form view, delete old version from list ONLY if changes are approved
         drawFormView(title, desc, indexPosition);
@@ -19,6 +35,7 @@ function fillTaskDesc(title, desc, priority, category, dueDate, indexPosition) {
     });
 
     const deleteTaskBtn = document.createElement('button');
+    deleteTaskBtn.classList.add('deleteTaskBtn');
     deleteTaskBtn.textContent = "Delete";
     deleteTaskBtn.addEventListener("click", function () {
         //console.log("DELETE" + indexPosition);
@@ -27,6 +44,11 @@ function fillTaskDesc(title, desc, priority, category, dueDate, indexPosition) {
         clearView();
         drawListView();
     });
+    taskDescColumn.appendChild(taskDetailName);
+    taskDescColumn.appendChild(taskDetailCategory);
+    taskDescColumn.appendChild(taskDetailPriority);
+    taskDescColumn.appendChild(taskDetailDueDate);
+    taskDescColumn.appendChild(taskDetailDescription);
     taskDescColumn.appendChild(editTaskBtn);
     taskDescColumn.appendChild(deleteTaskBtn);
 }
