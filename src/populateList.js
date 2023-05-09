a5bcbf4aeaabd949c1dee0242e56ec319a6e7290
jakebrowning90 <jacobbrowning90@gmail.com>
2023-05-09 11:18:14 -0400

    Implement basic localStorage for taskList

--- src/populateList.js
+++ src/populateList.js
@@ -1,6 +1,6 @@
 // Reads array of tasks and creates a div for each one
 
-import { taskList } from "./taskFactory";
+import { taskList, checkStorage } from "./taskFactory";
 import { fillTaskDesc } from "./fillTaskDesc";
 import { format } from 'date-fns';
 
@@ -10,55 +10,15 @@ function populateList(category) {
     while (displayedTasks.firstChild) {
         displayedTasks.removeChild(displayedTasks.lastChild);
     }
-    // Gets all tasks 
-    if (category == null) {
-        for (const task in taskList) {
-            let taskRow = document.createElement('div');
-            taskRow.classList.add('taskRow');
 
-            let taskRadioSpan = document.createElement('label');
-            let taskRowName = document.createElement('div');
-            taskRowName.classList.add('taskRowName');
-            let taskRowDate = document.createElement('div');
-            taskRowDate.classList.add('taskRowDate');
-            let taskRowSelectMark = document.createElement('div');
-            taskRowSelectMark.classList.add('taskRowDefaultMark');
-            let selectionRadio = document.createElement('input');
-            selectionRadio.setAttribute('type', 'radio');
-            selectionRadio.setAttribute('name', 'currentSelection');
+    taskList = checkStorage();
 
-            taskRowName.textContent = taskList[task].title;
-            //Testing date-fns
-            // console.log(format(new Date(taskList[task].dueDate), 'PP'));
-
-            //OLD WAY: taskRowDate.textContent = taskList[task].dueDate;
-            taskRowDate.textContent = format(new Date(taskList[task].dueDate), 'PP');
-
-            taskRow.appendChild(taskRowName);
-            taskRow.appendChild(taskRowDate);
-            taskRow.appendChild(taskRowSelectMark);
-            taskRowSelectMark.appendChild(selectionRadio);
-
-            taskRow.addEventListener("click", function () {
-                fillTaskDesc(taskList[task].title, taskList[task].description, taskList[task].priorityLevel, taskList[task].taskCategory, taskList[task].dueDate, task);
-                //TODO: function to change class of taskRowSelectMark, make current row YELLOW
-                // let unselectedTasks = document.querySelectorAll('taskRowDefaultMark');
-                // console.log(unselectedTasks);
-                // unselectedTasks.classList.remove('taskRowCurrentMark');
-                // taskRowSelectMark.classList.add('taskRowCurrentMark');
-            });
-            
-            taskRadioSpan.appendChild(taskRow);
-            displayedTasks.appendChild(taskRadioSpan);
-        } 
-    }
-    // Gets only tasks in chosen category
-    else {
-        for (const task in taskList) {
-                if (taskList[task].taskCategory == category) {
+        // Gets all tasks 
+        if (category == null) {
+            for (const task in taskList) {
                 let taskRow = document.createElement('div');
                 taskRow.classList.add('taskRow');
-                
+
                 let taskRadioSpan = document.createElement('label');
                 let taskRowName = document.createElement('div');
                 taskRowName.classList.add('taskRowName');
@@ -71,7 +31,12 @@ function populateList(category) {
                 selectionRadio.setAttribute('name', 'currentSelection');
 
                 taskRowName.textContent = taskList[task].title;
-                taskRowDate.textContent = taskList[task].dueDate;
+                //Testing date-fns
+                // console.log(format(new Date(taskList[task].dueDate), 'PP'));
+
+                //OLD WAY: taskRowDate.textContent = taskList[task].dueDate;
+                taskRowDate.textContent = format(new Date(taskList[task].dueDate), 'PP');
+
                 taskRow.appendChild(taskRowName);
                 taskRow.appendChild(taskRowDate);
                 taskRow.appendChild(taskRowSelectMark);
@@ -79,13 +44,52 @@ function populateList(category) {
 
                 taskRow.addEventListener("click", function () {
                     fillTaskDesc(taskList[task].title, taskList[task].description, taskList[task].priorityLevel, taskList[task].taskCategory, taskList[task].dueDate, task);
+                    //TODO: function to change class of taskRowSelectMark, make current row YELLOW
+                    // let unselectedTasks = document.querySelectorAll('taskRowDefaultMark');
+                    // console.log(unselectedTasks);
+                    // unselectedTasks.classList.remove('taskRowCurrentMark');
+                    // taskRowSelectMark.classList.add('taskRowCurrentMark');
                 });
-
+                
                 taskRadioSpan.appendChild(taskRow);
                 displayedTasks.appendChild(taskRadioSpan);
-            }
-        } 
-    }
+            } 
+        }
+        // Gets only tasks in chosen category
+        else {
+            for (const task in taskList) {
+                    if (taskList[task].taskCategory == category) {
+                    let taskRow = document.createElement('div');
+                    taskRow.classList.add('taskRow');
+                    
+                    let taskRadioSpan = document.createElement('label');
+                    let taskRowName = document.createElement('div');
+                    taskRowName.classList.add('taskRowName');
+                    let taskRowDate = document.createElement('div');
+                    taskRowDate.classList.add('taskRowDate');
+                    let taskRowSelectMark = document.createElement('div');
+                    taskRowSelectMark.classList.add('taskRowDefaultMark');
+                    let selectionRadio = document.createElement('input');
+                    selectionRadio.setAttribute('type', 'radio');
+                    selectionRadio.setAttribute('name', 'currentSelection');
+
+                    taskRowName.textContent = taskList[task].title;
+                    taskRowDate.textContent = taskList[task].dueDate;
+                    taskRow.appendChild(taskRowName);
+                    taskRow.appendChild(taskRowDate);
+                    taskRow.appendChild(taskRowSelectMark);
+                    taskRowSelectMark.appendChild(selectionRadio);
+
+                    taskRow.addEventListener("click", function () {
+                        fillTaskDesc(taskList[task].title, taskList[task].description, taskList[task].priorityLevel, taskList[task].taskCategory, taskList[task].dueDate, task);
+                    });
+
+                    taskRadioSpan.appendChild(taskRow);
+                    displayedTasks.appendChild(taskRadioSpan);
+                }
+            } 
+        }
+       
 }
 
 export { populateList };
--- src/taskFactory.js
+++ src/taskFactory.js
@@ -1,13 +1,14 @@
-let taskList = [
-    {"title": "Walk dog", "description": "Take Loki to play at the beach", "dueDate": "1 January 2023", "priorityLevel": "Normal", "taskCategory": "House"},
-    {"title": "Make valentine's gifts", "description": "Tools in garage", "dueDate": "14 February 2023", "priorityLevel": "Essential", "taskCategory": "House"},
-    {"title": "Bake cake", "description": "Chocolate cake for Flint's birthday", "dueDate": "12 March 2023", "priorityLevel": "Essential", "taskCategory": "Hobby"},
-    {"title": "Reply to Discord event", "description": "Mario Kart game night", "dueDate": "12 May 2023", "priorityLevel": "Normal", "taskCategory": "Hobby"},
-    {"title": "Practice German", "description": "Start new Anki deck", "dueDate": "11 June 2023", "priorityLevel": "Normal", "taskCategory": "Work"},
-    {"title": "Complete JavaScript exercise", "description": "Clone github repo", "dueDate": "5 August 2023", "priorityLevel": "Essential", "taskCategory": "Work"},
-    {"title": "Make dentist appointment", "description": "Get morning timeslot", "dueDate": "15 November 2023", "priorityLevel": "Normal", "taskCategory": "Hobby"},
-    {"title": "Shop for gift", "description": "Charity shops", "dueDate": "2 December 2023", "priorityLevel": "Normal", "taskCategory": "House"}
-];
+
+// let taskList = [
+//     // {"title": "Walk dog", "description": "Take Loki to play at the beach", "dueDate": "1 January 2023", "priorityLevel": "Normal", "taskCategory": "House"},
+//     // {"title": "Make valentine's gifts", "description": "Tools in garage", "dueDate": "14 February 2023", "priorityLevel": "Essential", "taskCategory": "House"},
+//     // {"title": "Bake cake", "description": "Chocolate cake for Flint's birthday", "dueDate": "12 March 2023", "priorityLevel": "Essential", "taskCategory": "Hobby"},
+//     // {"title": "Reply to Discord event", "description": "Mario Kart game night", "dueDate": "12 May 2023", "priorityLevel": "Normal", "taskCategory": "Hobby"},
+//     // {"title": "Practice German", "description": "Start new Anki deck", "dueDate": "11 June 2023", "priorityLevel": "Normal", "taskCategory": "Work"},
+//     // {"title": "Complete JavaScript exercise", "description": "Clone github repo", "dueDate": "5 August 2023", "priorityLevel": "Essential", "taskCategory": "Work"},
+//     // {"title": "Make dentist appointment", "description": "Get morning timeslot", "dueDate": "15 November 2023", "priorityLevel": "Normal", "taskCategory": "Hobby"},
+//     // {"title": "Shop for gift", "description": "Charity shops", "dueDate": "2 December 2023", "priorityLevel": "Normal", "taskCategory": "House"}
+// ];
 
 //TODO: completion status?
 class Task {
@@ -22,7 +23,16 @@ class Task {
 };
 
 function addTaskToList(Task) {
-    taskList.push(Task)
+    taskList.push(Task);
+    localStorage.setItem("taskList", JSON.stringify(taskList));
 }
 
-export { taskList, addTaskToList, Task };
+function checkStorage() {
+    if (JSON.parse(localStorage.getItem('taskList')) != null) {
+        return JSON.parse(localStorage.getItem('taskList'));
+    } else {
+        return [];
+    }
+}
+
+export { taskList, addTaskToList, checkStorage, Task };
